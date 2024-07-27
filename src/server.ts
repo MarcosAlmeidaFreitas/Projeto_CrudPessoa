83506664c7b46f52c598dd60e4e832fb6d4536cb
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-07-27 14:04:02 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient  } from "@prisma/client"
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+const app = fastify();
+
+app.post('/persons', (request, reply) => {
+  const createEventSchema = z.object({
+    name: z.string(),
+    cpf: z.string(), 
+    email: z.string().email(), 
+    image: z.string(),
+  })
+  
+  const data = createEventSchema.parse(request.body);
+  return 'hello nwl unite';
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP SERVER RUNNING');
+})
+
